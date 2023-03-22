49d876364168b2bef485bd41400192e9f7fc7459
DESKTOP-S7G141E\yuanhao-PC <fyuanhao91@gmail.com>
2023-03-22 20:25:25 +0800

    [hw1] indexed

--- hw1/hw1/main.cpp
+++ hw1/hw1/main.cpp
@@ -1,81 +1,120 @@
-#include <stdio.h>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
+using namespace std;
 using namespace cv;
 
-// 轉灰階
-Mat ConvertToGray(Mat colorImage) {
-    Mat grayImage(colorImage.size(), CV_8UC1);
+class ImageLibrary 
+{
+private:
+    Vec3b _colorMap[256];
 
-    for (int i = 0; i < colorImage.rows; i++)
-    {
-        for (int j = 0; j < colorImage.cols; j++)
+    void CreateColorMap() {
+        for (int i = 0; i < 256; i++)
+            _colorMap[i] = Vec3b(rand() % 256, rand() % 256, rand() % 256);
+    }
+
+public:
+    ImageLibrary() {
+        this->CreateColorMap();
+    }
+
+    // 轉灰階
+    Mat ConvertToGray(Mat colorImage) {
+        Mat grayImage(colorImage.size(), CV_8UC1);
+
+        for (int i = 0; i < colorImage.rows; i++)
         {
-            Vec3b pixel = colorImage.at<Vec3b>(i, j);
-            int grayValue = 0.3 * pixel[2] + 0.59 * pixel[1] + 0.11 * pixel[0];
-            grayImage.at<uchar>(i, j) = grayValue;
+            for (int j = 0; j < colorImage.cols; j++)
+            {
+                Vec3b pixel = colorImage.at<Vec3b>(i, j);
+                int grayValue = 0.3 * pixel[2] + 0.59 * pixel[1] + 0.11 * pixel[0];
+                grayImage.at<uchar>(i, j) = grayValue;
+            }
         }
+        return grayImage;
     }
-    return grayImage;
-}
 
-// 灰階二值化
-Mat ConvertToBinary(Mat grayImage, uchar threshold=128) {
-    Mat binaryImage(grayImage.size(), CV_8UC1);
-    
-    for (int i = 0; i < grayImage.rows; i++)
-    {
-        for (int j = 0; j < grayImage.cols; j++)
+    // 灰階二值化
+    Mat ConvertToBinary(Mat colorImage, uchar threshold = 128) {
+        Mat grayImage = this->ConvertToGray(colorImage);
+        Mat binaryImage(grayImage.size(), CV_8UC1);
+
+        for (int i = 0; i < grayImage.rows; i++)
         {
-            binaryImage.at<uchar>(i, j) = grayImage.at<uchar>(i, j) > threshold ? 255 : 0;
+            for (int j = 0; j < grayImage.cols; j++)
+            {
+                binaryImage.at<uchar>(i, j) = grayImage.at<uchar>(i, j) > threshold ? 255 : 0;
+            }
         }
-    }
-    return binaryImage;
-}
-
-// 轉 index-color image
-Mat ConvertToIndexColor(Mat colorImage) {
-    Mat colorMap(1, 256, CV_8UC3);
-    for (int i = 0; i < 256; i++) 
-    {
-        colorMap.at<Vec3b>(0, i) = Vec3b(i, i, i);
+        return binaryImage;
     }
 
-    Mat indexColorImage(colorImage.size(), CV_8UC1);
-    for (int i = 0; i < colorImage.rows; i++) {
-        for (int j = 0; j < colorImage.cols; j++) {
-            Vec3b color = colorImage.at<Vec3b>(i, j);
-            int index = (color[0] + color[1] + color[2]) / 3;
-            indexColorImage.at<Vec3b>(i, j) = colorMap.at<Vec3b>(0, index);
+    // 轉 indexed image
+    Mat ConvertToIndexedColor(Mat colorImage, Vec3b colorMap[256] = nullptr) {
+        if (colorMap == nullptr)
+            colorMap = this->_colorMap;
+
+        Mat indexImage(colorImage.size(), CV_8UC1);
+        Mat mappingImage(colorImage.size(), CV_8UC3);
+        const int MAX_DIST = 255.0 * 255.0 * 3.0;
+        for (int i = 0; i < colorImage.rows; i++) {
+            for (int j = 0; j < colorImage.cols; j++) {
+                Vec3b color = colorImage.at<Vec3b>(i, j);
+                uchar index = 0;
+                int minDist = MAX_DIST;
+                for (int k = 0; k < 256; k++) {
+                    Vec3b mapColor = colorMap[k];
+                    int dist =
+                        (color[0] - mapColor[0]) * (color[0] - mapColor[0]) +
+                        (color[1] - mapColor[1]) * (color[1] - mapColor[1]) +
+                        (color[2] - mapColor[2]) * (color[2] - mapColor[2]);
+                    if (dist < minDist) {
+                        minDist = dist;
+                        index = k;
+                    }
+                }
+                indexImage.at<uchar>(i, j) = index;
+                mappingImage.at<Vec3b>(i, j) = colorMap[index];
+            }
         }
+        return mappingImage;
     }
-    return indexColorImage;
-}
-
+};
 
 int main() {
-    const std::string PATHS[] = {
-        "..\\image\\House256.png",
-        "..\\image\\House512.png",
-        "..\\image\\JellyBeans.png",
-        "..\\image\\Lena.png",
-        "..\\image\\Mandrill.png",
-        "..\\image\\Peppers.png",
+    const string IMAGE_NAMES[] = {
+        "House256",
+        "House512",
+        "JellyBeans",
+        "Lena",
+        "Mandrill",
+        "Peppers",
     };
-
-    for (std::string path : PATHS)
+    
+    const string IMAGE_PATH_FORMAT = "..\\image\\%s.png";
+    for (string name : IMAGE_NAMES)
     {
-        Mat colorImage = imread(path);
-        Mat grayImage = ConvertToGray(colorImage);
-        Mat binaryImage = ConvertToBinary(grayImage, 128);
-        Mat indexColorImage = ConvertToIndexColor(colorImage);
+        const string IMAGE_PATH = format(IMAGE_PATH_FORMAT.c_str(), name.c_str());
+        cout << IMAGE_PATH << '\n';
 
-        imshow("true-color " + path, colorImage);
-        imshow("gray " + path, grayImage);
-        imshow("binary " + path, binaryImage);
-        imshow("index-color " + path, indexColorImage);
+        ImageLibrary library = ImageLibrary();
+        Mat colorImage = imread(IMAGE_PATH);
+        Mat grayImage = library.ConvertToGray(colorImage);
+        Mat binaryImage = library.ConvertToBinary(colorImage, 128);
+        Mat indexColorImage = library.ConvertToIndexedColor(colorImage);
+
+        imshow("true-color " + IMAGE_PATH, colorImage);
+        imshow("gray " + IMAGE_PATH, grayImage);
+        imshow("binary " + IMAGE_PATH, binaryImage);
+        imshow("indexed " + IMAGE_PATH, indexColorImage);
+
+        imwrite(format(IMAGE_PATH_FORMAT.c_str(), (name + "_gray").c_str()), grayImage);
+        imwrite(format(IMAGE_PATH_FORMAT.c_str(), (name + "_binary").c_str()), binaryImage);
+        imwrite(format(IMAGE_PATH_FORMAT.c_str(), (name + "_indexed").c_str()), indexColorImage);
+        
         waitKey(0);
+        destroyAllWindows();
     }
-    destroyAllWindows();
     return 0;
 }
