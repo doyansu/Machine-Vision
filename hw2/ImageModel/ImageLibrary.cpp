589cc6a9044cc87a65c4c9b9bf545a3ab7db5f1a
DESKTOP-S7G141E\yuanhao-PC <fyuanhao91@gmail.com>
2023-04-05 03:47:20 +0800

    [hw2] 4/4

--- hw2/ImageModel/ImageLibrary.cpp
+++ hw2/ImageModel/ImageLibrary.cpp
@@ -5,10 +5,10 @@ namespace image_model {
     Mat ImageLibrary::ConvertToGray(Mat colorImage) {
         Mat grayImage(colorImage.size(), CV_8UC1);
 
-        for (int row = 0; row < colorImage.rows; row++) {
-            for (int col = 0; col < colorImage.cols; col++) {
+        for (int row = 0; row < grayImage.rows; row++) {
+            for (int col = 0; col < grayImage.cols; col++) {
                 Vec3b pixel = colorImage.at<Vec3b>(row, col);
-                int grayValue = (int)(0.3 * pixel[2] + 0.59 * pixel[1] + 0.11 * pixel[0]);
+                int grayValue = (int)(0.34 * pixel[2] + 0.33 * pixel[1] + 0.33 * pixel[0]);
                 grayImage.at<uchar>(row, col) = grayValue;
             }
         }
@@ -20,8 +20,8 @@ namespace image_model {
         Mat grayImage = this->ConvertToGray(colorImage);
         Mat binaryImage(grayImage.size(), CV_8UC1);
 
-        for (int row = 0; row < grayImage.rows; row++)
-            for (int col = 0; col < grayImage.cols; col++)
+        for (int row = 0; row < binaryImage.rows; row++)
+            for (int col = 0; col < binaryImage.cols; col++)
                 binaryImage.at<uchar>(row, col) = grayImage.at<uchar>(row, col) > threshold ? 255 : 0;
         return binaryImage;
     }
@@ -29,8 +29,14 @@ namespace image_model {
     // Тр Labeling Image
     Mat ImageLibrary::ConvertToLabeling(Mat colorImage, Connected connected, int* objNumber){
         int count = 0;
-        Mat binaryImage = this->ConvertToBinary(colorImage);
+        Mat binaryImage = this->ConvertToBinary(colorImage, 160);
         Mat labelingImage(binaryImage.size(), CV_8UC3);
+
+        for (int row = 0; row < labelingImage.rows; row++)
+            for (int col = 0; col < labelingImage.cols; col++) {
+                int value = binaryImage.at<uchar>(row, col) ^ 255 ^ 0;
+                labelingImage.at<Vec3b>(row, col) = Vec3b(value, value, value);
+            }
 
         if (objNumber != nullptr)
             *objNumber = count;
