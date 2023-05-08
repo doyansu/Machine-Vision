a582934d810c0c9089f6ab124f65d8a8d2275dbc
DESKTOP-S7G141E\yuanhao-PC <fyuanhao91@gmail.com>
2023-05-08 21:30:52 +0800

    [hw4] filter class 結構

--- hw4/ImageModel/Main.cpp
+++ hw4/ImageModel/Main.cpp
@@ -1,39 +1,108 @@
 ﻿#include <iostream>
-#include <filesystem>
+#include <filesystem> // ISO C++17 標準 (/std:c++17)
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 namespace fs = std::filesystem;
 
-// Filter 類型
-enum class Filter {
-    Mean,
-    Median,
-    Gaussian,
+class Filter 
+{
+public:
+    Filter() {}
+
+    void SetMask(int mask) {
+        this->_mask = (mask < 3) ? 3 : (mask | 1); // 確保 mask 至少為3，且是奇數
+    }
+
+    virtual Mat FilterImage(const Mat& sourceImage) = 0;
+
+private:
+    int _mask = 3;
+};
+
+class MeanFilter : public Filter
+{
+public:
+    MeanFilter() {
+
+    }
+
+    Mat FilterImage(const Mat& sourceImage) override {
+
+    }
+};
+
+class MedianFilter : public Filter
+{
+public:
+    MedianFilter() {
+
+    }
+
+    Mat FilterImage(const Mat& sourceImage) override {
+
+    }
+};
+
+class GaussianFilter : public Filter
+{
+public:
+    GaussianFilter() {
+
+    }
+
+    Mat FilterImage(const Mat& sourceImage) override {
+
+    }
 };
 
 class ImageLibrary
 {
 public:
+    // Filter 類型
+    enum class FilterType {
+        Mean,
+        Median,
+        Gaussian,
+    };
+
     ImageLibrary() {
         
     }
 
-    Mat FilterBy(const Mat& sourceImage, Filter filter = Filter::Gaussian, unsigned int times = 1, int mask = 3) {
-        mask = (mask < 3) ? 3 : (mask|1); // 確保 mask 至少為3，且是奇數
+    // Filter
+    Mat FilterBy(const Mat& sourceImage, FilterType filterType = FilterType::Gaussian, unsigned int times = 1, int mask = 3) {
+        
         Mat resultImage = Mat(sourceImage.size(), CV_8UC3);
+        Filter *filter = this->CreateFilter(filterType);
+        filter->SetMask(mask);
         while (times--)
-        {
-            for (int i = 0; i < resultImage.rows; i++)
-                for (int j = 0; j < resultImage.cols; j++)
-                    resultImage.at<Vec3b>(i, j) = sourceImage.at<Vec3b>(i, j);
-        }
+            resultImage = filter->FilterImage(resultImage);
+        delete filter;
         return resultImage;
     }
 
 private:
-
+    Filter* CreateFilter(FilterType filterType) {
+        Filter* filter = nullptr;
+        switch (filterType)
+        {
+        case ImageLibrary::FilterType::Mean:
+            filter = new MeanFilter();
+            break;
+        case ImageLibrary::FilterType::Median:
+            filter = new MedianFilter();
+            break;
+        case ImageLibrary::FilterType::Gaussian:
+            filter = new GaussianFilter();
+            break;
+        default:
+            throw "error";
+            break;
+        }
+        return filter;
+    }
 };
 
 class ImageInfo 
@@ -51,21 +120,19 @@ public:
         return this->_path.stem().string();
     }
 
+    string Extension() {
+        return this->_path.extension().string();
+    }
+
 private:
     fs::path _path;
 };
 
-void ShowAndWrite(string name, Mat image) {
-    imshow(name, image);
-    imwrite(name + ".png", image);
-}
-
 int main() {
-    const int IMAGE_NUM = 4; // 圖片數
     const string IMAGE_FOLDER = "..\\image\\"; // 圖片存放資料夾
     vector<ImageInfo> images;
 
-    // 圖片檔名、threshold、layer 設定
+    // 圖片檔名設定
     images.push_back(ImageInfo(IMAGE_FOLDER + "House256_noise.png"));
     images.push_back(ImageInfo(IMAGE_FOLDER + "Lena_gray.png"));
     images.push_back(ImageInfo(IMAGE_FOLDER + "Mandrill_gray.png"));
@@ -73,22 +140,28 @@ int main() {
 
     ImageLibrary library = ImageLibrary();
 
-    for (ImageInfo image : images)
+    for (ImageInfo imageInfo : images)
     {
-        std::cout << image.Path() << '\n';
+        std::cout << imageInfo.Path() << '\n';
 
         // 讀取圖片
-        Mat sourceImage = imread(image.Path());
-        imshow(image.FileName(), sourceImage);
+        Mat sourceImage = imread(imageInfo.Path());
+        imshow(imageInfo.FileName(), sourceImage);
+
+        // 顯示並儲存圖片
+        auto showWriteImage = [extension = imageInfo.Extension()](string name, Mat image) {
+            imshow(name, image);
+            imwrite(name + extension, image);
+        };
 
         // Filter
         const int FILTER_TIMES = 7;
         for (int times = 1; times <= FILTER_TIMES; times++) {
-            ShowAndWrite(image.FileName() + " Mean mask3 times" + to_string(times), library.FilterBy(sourceImage, Filter::Mean, times, 3));
-            ShowAndWrite(image.FileName() + " Mean mask7 times" + to_string(times), library.FilterBy(sourceImage, Filter::Mean, times, 7));
-            ShowAndWrite(image.FileName() + " Median mask3 times" + to_string(times), library.FilterBy(sourceImage, Filter::Median, times, 3));
-            ShowAndWrite(image.FileName() + " Median mask7 times" + to_string(times), library.FilterBy(sourceImage, Filter::Median, times, 7));
-            ShowAndWrite(image.FileName() + " Gaussian mask5 times" + to_string(times), library.FilterBy(sourceImage, Filter::Gaussian, times, 5));
+            showWriteImage(imageInfo.FileName() + " Mean mask3 times" + to_string(times), library.FilterBy(sourceImage, ImageLibrary::FilterType::Mean, times, 3));
+            showWriteImage(imageInfo.FileName() + " Mean mask7 times" + to_string(times), library.FilterBy(sourceImage, ImageLibrary::FilterType::Mean, times, 7));
+            showWriteImage(imageInfo.FileName() + " Median mask3 times" + to_string(times), library.FilterBy(sourceImage, ImageLibrary::FilterType::Median, times, 3));
+            showWriteImage(imageInfo.FileName() + " Median mask7 times" + to_string(times), library.FilterBy(sourceImage, ImageLibrary::FilterType::Median, times, 7));
+            showWriteImage(imageInfo.FileName() + " Gaussian mask5 times" + to_string(times), library.FilterBy(sourceImage, ImageLibrary::FilterType::Gaussian, times, 5));
         }
 
         cv::waitKey(0);
