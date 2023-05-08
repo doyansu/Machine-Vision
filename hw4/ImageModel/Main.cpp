db0e7c2c781f6053d1f7dafee82b4d5ab7f8327c
doyansu <fyuanhao91@gmail.com>
2023-05-08 16:35:11 +0800

    [hw4] 圖片架構

--- hw4/ImageModel/Main.cpp
+++ hw4/ImageModel/Main.cpp
@@ -6,12 +6,34 @@ using namespace std;
 using namespace cv;
 namespace fs = std::filesystem;
 
+// Filter 類型
+enum class Filter {
+    Mean,
+    Median,
+    Gaussian,
+};
+
 class ImageLibrary
 {
 public:
     ImageLibrary() {
         
     }
+
+    Mat FilterBy(const Mat& sourceImage, Filter filter = Filter::Gaussian, unsigned int times = 1, int mask = 3) {
+        mask = (mask < 3) ? 3 : (mask|1); // 確保 mask 至少為3，且是奇數
+        Mat resultImage = Mat(sourceImage.size(), CV_8UC3);
+        while (times--)
+        {
+            for (int i = 0; i < resultImage.rows; i++)
+                for (int j = 0; j < resultImage.cols; j++)
+                    resultImage.at<Vec3b>(i, j) = sourceImage.at<Vec3b>(i, j);
+        }
+        return resultImage;
+    }
+
+private:
+
 };
 
 class ImageInfo 
@@ -33,6 +55,11 @@ private:
     fs::path _path;
 };
 
+void ShowAndWrite(string name, Mat image) {
+    imshow(name, image);
+    imwrite(name + ".png", image);
+}
+
 int main() {
     const int IMAGE_NUM = 4; // 圖片數
     const string IMAGE_FOLDER = "..\\image\\"; // 圖片存放資料夾
@@ -51,9 +78,18 @@ int main() {
         std::cout << image.Path() << '\n';
 
         // 讀取圖片
-        Mat colorImage = imread(image.Path());
+        Mat sourceImage = imread(image.Path());
+        imshow(image.FileName(), sourceImage);
 
-        imshow(image.FileName(), colorImage);
+        // Filter
+        const int FILTER_TIMES = 7;
+        for (int times = 1; times <= FILTER_TIMES; times++) {
+            ShowAndWrite(image.FileName() + " Mean mask3 times" + to_string(times), library.FilterBy(sourceImage, Filter::Mean, times, 3));
+            ShowAndWrite(image.FileName() + " Mean mask7 times" + to_string(times), library.FilterBy(sourceImage, Filter::Mean, times, 7));
+            ShowAndWrite(image.FileName() + " Median mask3 times" + to_string(times), library.FilterBy(sourceImage, Filter::Median, times, 3));
+            ShowAndWrite(image.FileName() + " Median mask7 times" + to_string(times), library.FilterBy(sourceImage, Filter::Median, times, 7));
+            ShowAndWrite(image.FileName() + " Gaussian mask5 times" + to_string(times), library.FilterBy(sourceImage, Filter::Gaussian, times, 5));
+        }
 
         cv::waitKey(0);
         cv::destroyAllWindows();
